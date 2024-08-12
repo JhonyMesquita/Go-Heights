2a350c08f4090124a39b55193db9991a41669ee2
Jhonx <jhony.mesquita.work@gmail.com>
2024-08-11 22:22:16 -0300

    server http

---
+++ cmd/wsrc/main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+
+	"github.com/JhonyMesquita/Go-Heights/internal/api"
+	"github.com/JhonyMesquita/Go-Heights/internal/store/pgstore"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+		panic(err)
+	}
+
+	ctx := context.Background()
+
+	pool, err := pgxpool.New(ctx, fmt.Sprintf(
+		"user=%s password=%s host=%s port=%s dbname=%s",
+		os.Getenv("WSGO_DATABASE_USER"),
+		os.Getenv("WSGO_DATABASE_PASSWORD"),
+		os.Getenv("WSGO_DATABASE_HOST"),
+		os.Getenv("WSGO_DATABASE_PORT"),
+		os.Getenv("WSGO_DATABASE_NAME"),
+	))
+	if err != nil {
+		panic(err)
+	}
+
+	defer pool.Close()
+
+	if err := pool.Ping(ctx); err != nil {
+		panic(err)
+	}
+
+	handler := api.NewHandler(pgstore.New(pool))
+
+	go func() {
+		if err := http.ListenAndServe(":8080", handler); err != nil {
+			if !errors.Is(err, http.ErrServerClosed) {
+				panic(err)
+			}
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+}

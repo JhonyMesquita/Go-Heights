16f943a2ff73434723b68da2456146a2c8ddb9a9
Jhonx <jhony.mesquita.work@gmail.com>
2024-08-14 00:22:41 -0300

    create handleCreateRoom

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Go-Heights
+
+- rodar as migrations : go run cmd/tools/terndotenv/main.go
+- rodar o projeto: go run cmd/wsrc/main.go
--- internal/api/api.go
+++ internal/api/api.go
@@ -2,6 +2,7 @@ package api
 
 import (
 	"context"
+	"encoding/json"
 	"errors"
 	"log/slog"
 	"net/http"
@@ -48,7 +49,7 @@ func NewHandler(q *pgstore.Queries) http.Handler {
 		MaxAge:           300,
 	}))
 
-	r.Get("/subscribe{room_id}", a.handleSubscribe)
+	r.Get("/subscribe/{room_id}", a.handleSubscribe)
 
 	r.Route("/api", func(r chi.Router) {
 		r.Route("/rooms", func(r chi.Router) {
@@ -117,7 +118,31 @@ func (h *apiHandler) handleSubscribe(w http.ResponseWriter, r *http.Request) {
 	delete(h.subscribers[rawRoomID], c)
 	h.mu.Unlock()
 }
-func (h *apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request)             {}
+func (h *apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request) {
+	type _body struct {
+		Theme string `json:"theme"`
+	}
+	var body _body
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, "invalid json", http.StatusBadRequest)
+		return
+	}
+
+	roomID, err := h.q.InsertRoom(r.Context(), body.Theme)
+	if err != nil {
+		slog.Error("failed to insert room", "error", err)
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	type response struct {
+		ID string `json:"id"`
+	}
+
+	data, _ := json.Marshal(response{ID: roomID.String()})
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
 func (h *apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request)               {}
 func (h *apiHandler) handleCreateMessage(w http.ResponseWriter, r *http.Request)          {}
 func (h *apiHandler) handleGetMessages(w http.ResponseWriter, r *http.Request)            {}

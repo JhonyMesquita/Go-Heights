ecf77c5ce02bf5f9f0469ba990e443fa42ea2014
Jhonx <jhony.mesquita.work@gmail.com>
2024-08-15 22:08:57 -0300

    created message to room

--- internal/api/api.go
+++ internal/api/api.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
-	"fmt"
 	"log"
 	"log/slog"
 	"net/http"
@@ -59,7 +58,7 @@ func NewHandler(q *pgstore.Queries) http.Handler {
 			r.Get("/", a.handleGetRooms)
 
 			r.Route("/{room_id}/messages", func(r chi.Router) {
-				r.Post("/", a.handleCreateMessage)
+				r.Post("/", a.handleCreateRoomMessage)
 				r.Get("/", a.handleGetMessages)
 
 				r.Route("/{message_id}", func(r chi.Router) {
@@ -76,10 +75,40 @@ func NewHandler(q *pgstore.Queries) http.Handler {
 	return a
 }
 
+const (
+	MessageKindMessageCreated = "message_created"
+)
+
+type MessageMessageCreated struct {
+	ID      string `json:"id"`
+	Message string `json:"message"`
+}
+
+type Message struct {
+	Kind   string `json:"kind"`
+	Value  any    `json:"value"`
+	RoomID string `json:"-"`
+}
+
+func (h *apiHandler) notifyClients(msg Message) {
+	h.mu.Lock()
+	defer h.mu.Unlock()
+
+	subscribers, ok := h.subscribers[msg.RoomID]
+	if !ok || len(subscribers) == 0 {
+		return
+	}
+
+	for conn, cancel := range subscribers {
+		if err := conn.WriteJSON(msg); err != nil {
+			slog.Error("failed to send message to client", "error", err)
+			cancel()
+		}
+	}
+}
+
 func (h *apiHandler) handleSubscribe(w http.ResponseWriter, r *http.Request) {
-	log.Println("Entering handleSubscribe")
 	rawRoomID := chi.URLParam(r, "room_id")
-	log.Println("rawRoomID:", rawRoomID)
 	roomID, err := uuid.Parse(rawRoomID)
 	if err != nil {
 		log.Println("Invalid room ID:", rawRoomID)
@@ -130,7 +159,6 @@ func (h *apiHandler) handleSubscribe(w http.ResponseWriter, r *http.Request) {
 }
 
 func (h *apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("---------------------------------- handleCreateRoom")
 	type _body struct {
 		Theme string `json:"theme"`
 	}
@@ -154,10 +182,60 @@ func (h *apiHandler) handleCreateRoom(w http.ResponseWriter, r *http.Request) {
 	data, _ := json.Marshal(response{ID: roomID.String()})
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(data)
-	log.Println("Room created with ID:", roomID.String())
 }
-func (h *apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request)               {}
-func (h *apiHandler) handleCreateMessage(w http.ResponseWriter, r *http.Request)          {}
+func (h *apiHandler) handleGetRooms(w http.ResponseWriter, r *http.Request) {}
+func (h *apiHandler) handleCreateRoomMessage(w http.ResponseWriter, r *http.Request) {
+	rawRoomID := chi.URLParam(r, "room_id")
+	roomID, err := uuid.Parse(rawRoomID)
+	if err != nil {
+		http.Error(w, "invalid room id", http.StatusBadRequest)
+		return
+	}
+
+	_, err = h.q.GetRoom(r.Context(), roomID)
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			http.Error(w, "room not found", http.StatusNotFound)
+			return
+		}
+
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	type _body struct {
+		Message string `json:"message"`
+	}
+	var body _body
+	if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+		http.Error(w, "invalid json", http.StatusBadRequest)
+		return
+	}
+
+	messageID, err := h.q.InsertMessage(r.Context(), pgstore.InsertMessageParams{RoomID: roomID, Message: body.Message})
+	if err != nil {
+		slog.Error("failed to insert message", "error", err)
+		http.Error(w, "something went wrong", http.StatusInternalServerError)
+		return
+	}
+
+	type response struct {
+		ID string `json:"id"`
+	}
+
+	data, _ := json.Marshal(response{ID: messageID.String()})
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+
+	go h.notifyClients(Message{
+		Kind:   MessageKindMessageCreated,
+		RoomID: rawRoomID,
+		Value: MessageMessageCreated{
+			ID:      messageID.String(),
+			Message: body.Message,
+		},
+	})
+}
 func (h *apiHandler) handleGetMessages(w http.ResponseWriter, r *http.Request)            {}
 func (h *apiHandler) handleGetRoomMessage(w http.ResponseWriter, r *http.Request)         {}
 func (h *apiHandler) handleReactToMessage(w http.ResponseWriter, r *http.Request)         {}
